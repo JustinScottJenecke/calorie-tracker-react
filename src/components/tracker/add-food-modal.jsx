9071cddf603e9e8241070d6762e5748bf86bc90f
Justin <justinjenecke@gmail.com>
2023-11-06 19:55:35 +0200

    adds basic layout to add-food-modal

--- src/components/tracker/add-food-modal.jsx
+++ src/components/tracker/add-food-modal.jsx
@@ -2,7 +2,18 @@ const AddFoodModal = () => {
 
     return (
         <div>
-            Add Food
+            <form action="" method="get" className="is-flex-direction-column">
+                <div className="columns">
+                    <button className="button column">
+                        Select Food Item from manifest
+                    </button>
+                </div>
+                <div className="columns">
+                    <button className="button column">
+                        Enter new custom Food Item
+                    </button>
+                </div>
+            </form>
         </div>
     )
 }
