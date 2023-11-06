5dc280ef302d5e0fd6751d8afa32bea3ead5d804
Justin <justinjenecke@gmail.com>
2023-11-06 19:49:58 +0200

    creates skeleton ui for daily tracker and tracker display

--- src/components/tracker/tracker-display.jsx
+++ src/components/tracker/tracker-display.jsx
@@ -1,9 +1,47 @@
 const TrackerDisplay = () => {
 
+    // default unit is kj
+    const energy = 0
+
+    const macroNutrients = {
+        protein: 0,
+        fats: 0,
+        carbohydrates: 0
+    }
+
     return (
-        <div>
-            Kilojules and Macro-Nutrients
-        </div>
+        <header>
+
+            <section>
+                <h2 className="subtitle">
+                    Energy and Macronutrients
+                </h2>
+            </section>
+
+            <section className="columns">
+                <div className="column is-4 is-flex-direction-column">
+                    <div>Total</div>
+                    <div>0</div>
+                </div>
+                <div className="column is-8">
+                    <ul className="columns">
+                        <div className="column is-flex-direction-column">
+                            <div>Protein</div>
+                            <div>0</div>
+                        </div>
+                        <div className="column is-flex-direction-column">
+                            <div>Fats</div>
+                            <div>0</div>
+                        </div>
+                        <div className="column is-flex-direction-column">
+                            <div>Carbohydrates</div>
+                            <div>0</div>
+                        </div>
+                    </ul>
+                </div>
+            </section>
+
+        </header>
     )
 }
 
--- src/pages/daily-tracker.jsx
+++ src/pages/daily-tracker.jsx
@@ -4,11 +4,39 @@ import AddFoodModal from "../components/tracker/add-food-modal";
 
 const DailyTracker = () => {
 
+    const createFood = (name, energy, weight, protein, fats, carbs) => {
+        return {
+            // getName() {return name},
+            // getEnergy() {return energy},
+            // getWeight() {return weight},
+            // getProtein() {return protein},
+            // getFats() {return fats},
+            // getCarbs() {return carbs}
+            name: name,
+            energy: energy,
+            weight: weight,
+            macros: {
+                protein: protein,
+                fats: fats,
+                carbs: carbs
+            }
+        }
+    }
+
+
+    const dummy = {
+        protein: 0,
+        fats: 0,
+        carbs: 0
+    }
+
     return (
         <main>
-            <h2>Daily View</h2>
-            <TrackerDisplay />
+            <h2 className="title">Daily View</h2>
+            <TrackerDisplay/>
+            <hr />
             <TrackerFoodItemList />
+            <hr />
             <AddFoodModal />
         </main>
     )
