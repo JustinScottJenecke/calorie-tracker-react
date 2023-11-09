59219007066dc98f8357ad246548925a4918cd45
Justin <justinjenecke@gmail.com>
2023-11-09 07:03:58 +0200

    exports foodFactory function and adds state trackers to daily tracker for food, macros, energy

--- src/functions/daily-tracker.js
+++ src/functions/daily-tracker.js
@@ -5,3 +5,22 @@
  const defaultFunction = () => {
 
 }
+
+export const createFoodFactory = (name, energy, weight, protein, fats, carbs) => {
+    return {
+        // getName() {return name},
+        // getEnergy() {return energy},
+        // getWeight() {return weight},
+        // getProtein() {return protein},
+        // getFats() {return fats},
+        // getCarbs() {return carbs}
+        name: name,
+        energy: energy,
+        weight: weight,
+        macros: {
+            protein: protein,
+            fats: fats,
+            carbs: carbs
+        }
+    }
+}
--- src/pages/daily-tracker.jsx
+++ src/pages/daily-tracker.jsx
@@ -2,9 +2,19 @@ import TrackerDisplay from "../components/tracker/tracker-display";
 import TrackerFoodItemList from "../components/tracker/tracker-food-item-list";
 import AddFoodModal from "../components/tracker/add-food-modal";
 
+import { useState } from "react";
+
 const DailyTracker = () => {
 
-    const createFood = (name, energy, weight, protein, fats, carbs) => {
+    const [dailyEnergy, updateEnergy] = useState(0)
+    const [dailyMacros, updateMacros] = useState({
+        protein: 0,
+        fats: 0,
+        carbs: 0
+    })
+    const [dailyFoodItems, updateFoodItems] = useState([])
+
+    const createFoodFactory = (name, energy, weight, protein, fats, carbs) => {
         return {
             // getName() {return name},
             // getEnergy() {return energy},
@@ -23,21 +33,20 @@ const DailyTracker = () => {
         }
     }
 
-
-    const dummy = {
-        protein: 0,
-        fats: 0,
-        carbs: 0
+    const addItem = (energy) => {
+        updateEnergy(dailyEnergy + energy)
     }
 
     return (
         <main>
             <h2 className="title">Daily View</h2>
             <TrackerDisplay/>
+            {dailyEnergy}
             <hr />
             <TrackerFoodItemList />
             <hr />
             <AddFoodModal />
+            <button className="button" onClick={() => {addItem(500)}}>Add 100 kj</button>
         </main>
     )
 }
