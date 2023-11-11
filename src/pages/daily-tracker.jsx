24375d53a6afd18c1aaa265f04b92d2a8e29a013
Justin <justinjenecke@gmail.com>
2023-11-11 10:04:36 +0200

    adds functional implmentations for incrementing state of macros and kj using previous state function

--- src/pages/daily-tracker.jsx
+++ src/pages/daily-tracker.jsx
@@ -8,37 +8,31 @@ const DailyTracker = () => {
 
     // State Variables
     const [dailyEnergy, setEnergy] = useState(0)
-    const [dailyMacros, setMacros] = useState({
-        protein: 0,
-        fats: 0,
-        carbs: 0
-    })
+    const [dailyProtein, setProtein] = useState(0)
+    const [dailyFats, setFats] = useState(0)
+    const [dailyCarbs, setCarbs] = useState(0)
 
 
     const [dailyFoodItems, setFoodItems] = useState(['oats', 'chicken', 'broccoli', 'rice'])
 
     // Business Logic
-
-    // junk code: not reccomended to use individual functions since objects in state should not be mutated
-    // and should be updated as a whole
-    
-    // const addFats = (fats) => {
-    //     setEnergy(dailyEnergy + energy)
-    // }
-
-    // const addCarbs = (carbs) => {
-    //     setEnergy(dailyEnergy + energy)
-    // }
-
     const addItem = (energy) => {
-        setEnergy(dailyEnergy + energy)
+        setEnergy(prev => prev + energy)
     }
 
-    const addToMacros = (protein, fats, carbs) => {
+    const addFats = (fats) => {
+        setFats(prev => prev + fats)
+    }
 
-        setMacros( prevMacros => {
-            prevMacros.protein + 20
+    const addCarbs = (carbs) => {
+        setCarbs(prev => {
+            return prev + carbs
         })
+    }
+
+    const addProtein = (protein) => {
+
+        setProtein( prev => prev + protein)
     }
 
     const addMockFoodItem = (FoodItem, energy, protein, fats, carbs) => {
@@ -49,29 +43,27 @@ const DailyTracker = () => {
         <main>
             <h2 className="title">Daily View</h2>
             <TrackerDisplay/>
-        
             {dailyEnergy}
             <hr />
-            Daily Protein: {dailyMacros.protein}
+            Daily Protein: {dailyProtein}
             <br />
-            Daily Fats: {dailyMacros.fats}
+            Daily Fats: {dailyFats}
             <br />
-            Daily Carbohydrates: {dailyMacros.carbs}
-
-            {/* <hr />
+            Daily Carbohydrates: {dailyCarbs}
+            <hr />
             Daily Food: {
                 dailyFoodItems.map(foodItem => {
                     return <li key={foodItem}>{foodItem}</li>
                 })
-            } */}
+            }
             <hr />
             <TrackerFoodItemList />
             <hr />
             <AddFoodModal />
             <button className="button" onClick={() => {addItem(500)}}>Add 100 kj</button>
-            <button className="button" onClick={() => {addToMacros(10,0,0)}}>Add 10 Protein</button>
-            <button className="button" onClick={() => {addToMacros(0,10,0)}}>Add 10 Fats</button>
-            <button className="button" onClick={() => {addToMacros(0,0,10)}}>Add 10 Carbs</button>
+            <button className="button" onClick={() => {addProtein(10)}}>Add 10 Protein</button>
+            <button className="button" onClick={() => {addFats(10)}}>Add 10 Fats</button>
+            <button className="button" onClick={() => {addCarbs(10)}}>Add 10 Carbs</button>
             {/* <button className="button" onClick={() => {(500)}}>Add mock food item</button> */}
         </main>
     )
