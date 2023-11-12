64494d3101452b8c5b044c300d18ae518d0a2495
Justin <justinjenecke@gmail.com>
2023-11-12 23:07:15 +0200

    adds functionality for appending food-item to list in state

--- src/pages/daily-tracker.jsx
+++ src/pages/daily-tracker.jsx
@@ -14,16 +14,16 @@ const DailyTracker = () => {
 
     const [dailyFoodItems, setFoodItems] = useState([])
 
-    useEffect( () => {
-        fetch('src/data/food-items.json',{
-            headers : { 
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'
-             }
-          })
-          .then(response => response.json())
-         // .then(data => console.log(data))
-          .then(data => setFoodItems(data))
+    useEffect(() => {
+        fetch('src/data/food-items.json', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            // .then(data => console.log(data))
+            .then(data => setFoodItems(data))
     }, [])
 
     // ========= Business Logic =========
@@ -41,21 +41,33 @@ const DailyTracker = () => {
         })
     }
     const addProtein = (protein) => {
-        setProtein( prev => prev + protein)
+        setProtein(prev => prev + protein)
     }
-    // const addMockFoodItem = (FoodItem, energy, protein, fats, carbs) => {
-    //     setEnergy(dailyEnergy + energy)
-    // }
 
     // Food Item
     const addFoodItem = (mockItem) => {
+
+        mockFoodItem.id = dailyFoodItems.length
         setFoodItems([...dailyFoodItems, mockItem])
+    }
+
+    const mockFoodItem = {
+        name: "Sweet Potato",
+        kjPer100G: 360,
+        unit: "100-grams",
+        category: "starch",
+        serving: "30-grams",
+        macros: {
+            protein: 1.6,
+            fats: 0,
+           carbohydrates: 20
+        }
     }
 
     return (
         <main>
             <h2 className="title">Daily View</h2>
-            <TrackerDisplay/>
+            <TrackerDisplay />
             {dailyEnergy}
             <hr />
             Daily Protein: {dailyProtein}
@@ -73,11 +85,11 @@ const DailyTracker = () => {
             {/* <TrackerFoodItemList /> */}
             <hr />
             {/* <AddFoodModal /> */}
-            <button className="button" onClick={() => {addFoodItem('potato')}}>Add Food</button>
-            <button className="button" onClick={() => {addEnergy(500)}}>Add 100 kj</button>
-            <button className="button" onClick={() => {addProtein(10)}}>Add 10 Protein</button>
-            <button className="button" onClick={() => {addFats(10)}}>Add 10 Fats</button>
-            <button className="button" onClick={() => {addCarbs(10)}}>Add 10 Carbs</button>
+            <button className="button" onClick={() => { addFoodItem(mockFoodItem) }}>Add Food</button>
+            <button className="button" onClick={() => { addEnergy(500) }}>Add 100 kj</button>
+            <button className="button" onClick={() => { addProtein(10) }}>Add 10 Protein</button>
+            <button className="button" onClick={() => { addFats(10) }}>Add 10 Fats</button>
+            <button className="button" onClick={() => { addCarbs(10) }}>Add 10 Carbs</button>
             {/* <button className="button" onClick={() => {(500)}}>Add mock food item</button> */}
         </main>
     )
