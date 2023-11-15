306ed1e90dd2384701e3e1fdf84dc32bb2efe982
Justin <justinjenecke@gmail.com>
2023-11-15 16:09:10 +0200

    incorporates state into test cases but causes bugs with updating objects without reference

--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -1,56 +1,83 @@
+import { useState } from "react";
 import { energyAndMacroConverter } from "../../functions/daily-tracker";
 
 const TEST = () => {
 
-    const mockObjectGrams = {
-        "name": "Ground Beef",
-        "energy": 850,
-        "unit": "100-grams",
-        "category": "meat",
-        "serving": "",
-        "macros": {
-            "protein": 18.2,
-            "fats": 14.1,
-            "carbohydrates": 1
+    const mockFood = [
+        {
+            "id": 0,
+            "name": "Ground Beef",
+            "energy": 850,
+            "unit": "100-grams",
+            "category": "meat",
+            "serving": "",
+            "macros": {
+                "protein": 18.2,
+                "fats": 14.1,
+                "carbohydrates": 1
+            }
+        },
+        {
+            "id": 1,
+            "name": "Milk",
+            "energy": 200,
+            "unit": "100-ml",
+            "category": "dairy",
+            "serving": "",
+            "macros": {
+                "protein": 10,
+                "fats": 2,
+                "carbohydrates": 0.1
+            }
+        },
+        {
+            "id": 2,
+            "name": "Egg (whole)",
+            "energy": 320,
+            "unit": "1-whole",
+            "category": "egg",
+            "serving": "1",
+            "macros": {
+                "protein": 6.3,
+                "fats": 5,
+                "carbohydrates": 0.4
+            }
         }
-    }
+    ]
 
-    const mockObjectLitres = {
-        "name": "Milk",
-        "energy": 200,
-        "unit": "100-ml",
-        "category": "dairy",
-        "serving": "",
-        "macros": {
-            "protein": 10,
-            "fats": 2,
-            "carbohydrates": 0.1
-        }
-    }
+    const [testFoodRepo, setTestFoodRepo] = useState(mockFood)
+    const [testFoodItems, setTestFoodItems] = useState([])
 
-    const mockObjectWhole = {
-        "id": 2,
-        "name": "Egg (whole)",
-        "energy": 320,
-        "unit": "1-whole",
-        "category": "egg",
-        "serving": "1",
-        "macros": {
-            "protein": 6.3,
-            "fats": 5,
-            "carbohydrates": 0.4
-        }
-    }
-
-    const convertedObject = energyAndMacroConverter(mockObjectGrams, 100)
-
-    console.log(convertedObject)
 
     return (
         <>
             <p>
                 Test
             </p>
+            <button onClick={ () => {
+                let convertedObject = energyAndMacroConverter(testFoodRepo[1], 200)
+                
+                convertedObject.id = testFoodItems.length
+
+                setTestFoodItems([...testFoodItems,convertedObject])
+                }
+            }>
+                Test Set Food Items
+            </button>
+            <ul>
+                {
+                    testFoodItems.map( testFood => {
+                        return (
+                            <li key={testFood.id}>
+                                {testFood.id} -
+                                 {testFood.name}
+                                {testFood.energy}
+                                {testFood.unit}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </>
     )
 
