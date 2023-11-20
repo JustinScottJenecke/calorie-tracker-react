ff803ae78afc4e0fe8c5adf1fcd76640259efda7
Justin <justinjenecke@gmail.com>
2023-11-20 14:49:19 +0200

    creates FoodItem template for use in repostory list

--- src/components/food-item/index.jsx
+++ src/components/food-item/index.jsx
@@ -10,12 +10,26 @@ const FoodItem = (props) => {
     const [servingSizeState, setServingSizeState] = useState(servingSize)
 
     return (
-        <li>
-            {foodItem.id}
-            - {foodItem.name}
-            - <input type="number" value={servingSizeState} onChange={(e) => {setServingSizeState(e.target.value)}} /> {servingUnit}
-            - <button onClick={() => { props.addBtnHandler(foodItem.id, servingSizeState) }}>Add</button>
-        </li>
+        <div className="box">
+            <ul>
+            {/* <li>ID: {foodItem.id}</li> */}
+            <li className="subtitle has-text-centered">{foodItem.name}</li>
+            <li>Details:</li>
+            <li>Energy: {foodItem.energy}kj per {foodItem.unit}</li>
+            <li>Category: {foodItem.category}</li>
+            <li className="mt-4">Macros:</li>
+                <ul className="columns">
+                    <li className="column">Protein: {foodItem.macros.protein}g</li>
+                    <li className="column">Fats: {foodItem.macros.fats}g</li>
+                    <li className="column">Carbohydrates: {foodItem.macros.carbohydrates}g</li>
+                </ul>
+            </ul>
+            <div className="mt-4">
+                <input type="number" value={servingSizeState} onChange={(e) => {setServingSizeState(e.target.value)}} /> {servingUnit}
+                <br />
+                <button className="button is-success mt-2" onClick={() => { props.addBtnHandler(foodItem.id, servingSizeState) }}>Add</button>
+            </div>
+        </div>
     )
 }
 
