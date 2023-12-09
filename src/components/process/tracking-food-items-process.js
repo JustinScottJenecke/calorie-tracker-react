ab913c5a2ccfb579d9cdca55d43198a0e6094a46
Justin <justinjenecke@gmail.com>
2023-12-09 20:41:48 +0200

    Create tracking-food-items-process.js

---
+++ src/components/process/tracking-food-items-process.js
@@ -0,0 +1,17 @@
+
+/**
+ * Process component serves as a controller for component cycling when a user is
+ * attempting to add a food item to the list of tracked food items. Is able to track state
+ * of which component needs to be displayed/
+ * @returns {JSX}
+ */
+const TrackingFoodItemsProcess = () => {
+
+    return (
+        <section>
+            processes go here
+        </section>
+    )
+}
+
+export default TrackingFoodItemsProcess
