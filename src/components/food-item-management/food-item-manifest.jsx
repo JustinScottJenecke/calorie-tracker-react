4d8e34b9efbb0e054b6e7b034caa40bdf37f5cbc
Justin <justinjenecke@gmail.com>
2023-12-16 16:15:26 +0200

    adds basic docs to FoodItemManifest

--- src/components/food-item-management/food-item-manifest.jsx
+++ src/components/food-item-management/food-item-manifest.jsx
@@ -1,4 +1,14 @@
-const FoodItemManifest = () => {
+
+/**
+ * Component to display the unmodified/untracked food items stored inside of the applications
+ * main datastore. Displays a list of FoodItem components with more details than what is displayed 
+ * the daily tracker view
+ * @param {object} props 
+ * @param {object} props 
+ * @param {object} props 
+ * @returns 
+ */
+const FoodItemManifest = (props) => {
 
     return (
         <div>
